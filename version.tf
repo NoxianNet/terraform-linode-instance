1f445c71954373c5249ab415fa1f55b6452fab9a
Paul <paul@noxian.net>
2023-02-15 01:01:20 -0500

    initial code

---
+++ version.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    linode = {
+      source = "linode/linode"
+      version = ">= 1.30.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "2.0.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
