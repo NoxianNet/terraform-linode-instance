f47e7e18cce08dfbc2ed10412b23e4d6b81f4632
Paul <paul@noxian.net>
2023-02-16 18:01:59 -0500

    description to outputs #major

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,21 @@
 output "deployment_id" {
   value = local.deployment_id
+  description = "The deployment ID of the linode instance given by this module"
 }
 
 output "instance_id" {
   value = linode_instance.instance.id
+  description = "The ID of the Linode instance"
 }
 
 output "password" {
   value = var.root_password != "" ? var.root_password : random_password.root_password.result
   sensitive = true
+  description = "The root password"
 }
 
 output "private_ssh_key" {
   value = tls_private_key.ssh.private_key_pem
   sensitive = true
+  description = "the private ssh key"
 }
