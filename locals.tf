d18b7b5d42be6ee43bd85edf5621d90711ecf3d3
Paul Cruz <TheNite@users.noreply.github.com>
2024-01-26 02:11:03 -0500

    name fixes (#14)

--- locals.tf
+++ locals.tf
@@ -1,14 +1,13 @@
 locals {
   deployment_id = random_string.suffix.result
-  name_addition = var.name_addition == "" ? "" : var.name_addition
-  name          = "${local.name_addition}-${local.deployment_id}"
+  name          = var.name == "" ? local.deployment_id : "${var.name}-${local.deployment_id}"
   default_tags  = [for k, v in local.labels : "${k}: ${v}"]
   tags          = var.additional_tags == "" ? local.default_tags : concat([for k, v in var.additional_tags : "${k}: ${v}"], local.default_tags)
   labels = {
     terraform     = "true"
     deployment_id = local.deployment_id
     instance_name = local.name
-    location        = var.location
+    location      = var.location
     machine_type  = var.machine_type
   }
 }
